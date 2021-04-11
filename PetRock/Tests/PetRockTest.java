caf41f8a29c85292369dee9d724e1a8507516843
beboantonio <beboantonio1@hotmail.com>
2021-04-11 20:45:53 -0300

    add PetRock unit testing

---
+++ PetRock/Tests/PetRockTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PetRockTest {
+    private final PetRock rocky = new PetRock("Rocky");
+
+    @Test
+    public void getName() {
+        assertEquals("Rocky", rocky.getName());
+    }
+
+    @Test
+    public void testUnhappyToStart() {
+        assertFalse(rocky.isHappy());
+    }
+
+    @Test
+    public void testHappyAfterPlay() {
+        rocky.playWithRock();
+        assertTrue(rocky.isHappy());
+    }
+
+}
